e219959df6841513badf62e28e9da0349cd5c2a6
maxcelant <92411137+maxcelant@users.noreply.github.com>
2022-11-07 13:32:23 -0600

    Create logistic_regression_cost_function.py

---
+++ logistic_regression_cost_function.py
@@ -0,0 +1,12 @@
+def cost_function(X, y, w, b):
+  import numpy as np
+
+  total_cost = 0
+  for i in range(m):                                           # simplified cost function
+    z = np.dot(w, X[i]) + b                                    # calculate prediction z(i)
+    g = 1 / (1 + np.exp(-z))                                   # use sigmoid func to get g(i)
+    y_hat = g                                                  # prediction equal to g(z(i))
+    loss = -y[i] * np.log(y_hat) - ((1 - y[i]) * np.log(1 - y_hat)) # loss func
+    total_cost += loss                                         # add to total cost
+  total_cost =/ m                                              # divide by number of examples
+  return total_cost
